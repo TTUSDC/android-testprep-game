592a4eec59e2e41d817f01f3ec7542ae0e0bb059
Dane Lowrey <dane.r.lowrey@gmail.com>
2017-03-06 17:09:18 -0600

    Added BaseActivity class and adjusted other classes to inherit

---
+++ app/src/main/java/org/sdc/ttu/sdc_test_game_app/BaseActivity.java
@@ -0,0 +1,29 @@
+package org.sdc.ttu.sdc_test_game_app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Dane Lowrey on 3/6/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
