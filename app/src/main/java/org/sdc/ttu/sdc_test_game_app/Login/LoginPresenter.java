a81b1b797f040c7a6a5429cb6ce877b92f9a671d
dlowrey <dane.r.lowrey@gmail.com>
2017-04-26 18:57:16 -0500

    Merged homescreen branch

--- app/src/main/java/org/sdc/ttu/sdc_test_game_app/Login/LoginPresenter.java
+++ app/src/main/java/org/sdc/ttu/sdc_test_game_app/Login/LoginPresenter.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import org.sdc.ttu.sdc_test_game_app.CreateAccount.CreateAccountActivity;
+import org.sdc.ttu.sdc_test_game_app.HomeScreen.HomeScreenActivity;
 
 
 class LoginPresenter implements LoginInterface.Presenter {
